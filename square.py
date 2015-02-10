0bbf5be5fd6b7c6f308275a10b256bd0620012ff
nl1010 <yanlu1990@hotmail.co.uk>
2015-02-10 22:04:48 +0000

    written square script for the robot ,delete redundant file

--- square.py
+++ square.py
@@ -1,15 +1,43 @@
 from utils import *
 
-SQUARE_LOG_FILE_NAME = "square"
+def main():
+	SQUARE_LOG_FILE_NAME = "square"
 
-interface.startLogging("/home/pi/BrickPi/Logfiles/" + SQUARE_LOG_FILE_NAME)
+	#initialized brickpi interface 
+	interface = brickpi.Interface()
+	interface.initialize() 
 
-# Move 10cm forward
-move(5)
+	#define motor instances 
+	motors = [0,1]
+	
+	#using motorEnable to activate motor 
+	interface.motorEnable(motors[0])
+	interface.motorEnable(motors[1])
 
-# Rotate pi radians clockwise
-rotate(pi)
+	#set up  motorparameter from the value of interface <?> what is brickpi interface ? 
+	motorParams = setup(interface)
+	robot_until = RobotUtility() #where does this method comes from?
 
-interface.stopLogging()
+	#manully set drift callibration for test 
+	if len(sys.argv) == 4:
+    	robot_util.set_drift_callibration(float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]))
+    else :
+    	print "no drift calibration values set "
 
-interface.terminate()
+	#square move script  
+	#verbose : switch on/off debug information
+	robot_util.move (15,interface,motorParams,verbose = True)
+	robot_util.rotate(pi/2, interface, motorParams, verbose=True)
+	robot_util.move (15,interface,motorParams,verbose = True)
+	robot_util.rotate(pi/2, interface, motorParams, verbose=True)
+	robot_util.move (15,interface,motorParams,verbose = True)
+	robot_util.rotate(pi/2, interface, motorParams, verbose=True)
+	robot_util.move (15,interface,motorParams,verbose = True)
+	robot_util.rotate(pi/2, interface, motorParams, verbose=True)
+	
+	#termination
+	interface.terminate()
+
+	#<?> what is this ?
+	if __name__ == "__main__":
+		main()
