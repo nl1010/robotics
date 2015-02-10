5935db120baf16511632d818225788e05a96378e
nl1010 <yanlu1990@hotmail.co.uk>
2015-02-10 21:37:56 +0000

    config move action in utils

--- utils.py
+++ utils.py
@@ -34,23 +34,43 @@ motorParam1.pidParameters.k_p = 20.0
 motorParam1.pidParameters.k_i = 0.0 
 motorParam1.pidParameters.k_d = 0.0 
 
-'''Move distance cm forward'''
-def move(distance):
-  interface.setMotorAngleControllerParameters(motors[0],motorParam0)
-  interface.setMotorAngleControllerParameters(motors[1],motorParam1)
+# '''Move distance cm forward'''
+# def move(distance):
+#   interface.setMotorAngleControllerParameters(motors[0],motorParam0)
+#   interface.setMotorAngleControllerParameters(motors[1],motorParam1)
 
-  interface.increaseMotorAngleReferences(motors,[distance/WHEEL_RADIUS,distance/WHEEL_RADIUS])
+#   interface.increaseMotorAngleReferences(motors,[distance/WHEEL_RADIUS,distance/WHEEL_RADIUS])
   
-  while not interface.motorAngleReferencesReached(motors) :
-    motorAngles = interface.getMotorAngles(motors)
-    if motorAngles :
-      print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
+#   while not interface.motorAngleReferencesReached(motors) :
+#     motorAngles = interface.getMotorAngles(motors)
+#     if motorAngles :
+#       print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
 
+#My intergration of move 
+def move (distance)(self,distance,interface ,motorParams,verbose=True):
+  motors = [0,1]
+  # to control the motor angle correctly ,
+  #we need set up motor angle control parameters to each motor , the parametar are pre defined above 
+  interface.setMotorAngleControllerParameters(0,motorParam0)
+  interface.setMotorAngleControllerParameters(0.motorParam1)
+
+  # set target by using equation (?)
+  interface.increaseMotorAngleReferences(motors, [(distance * )])
+
+  #start process to approach the target by using While Not 
+  while not interface.motorAngleReferencesReached (motors) :
+    motorAngles = interface.getMotorAngles([0,1])
+    if motorAngles  and verbose :
+      print "motor angles :" , motorAngles[0][0],",",motorAngles[1][0]
+
+  while not interface.motorAngleReferencesReached
 '''Rotate radians clockwise'''
 def rotate(radians):
+  motors 0,1
   interface.setMotorAngleControllerParameters(motors[0],motorParam0)
   interface.setMotorAngleControllerParameters(motors[1],motorParam1)
   
+
   angle = ROBOT_RADIUS * pi / WHEEL_RADIUS
 
   interface.increaseMotorAngleReferences(motors, [angle, -angle])
