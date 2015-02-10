e56a8152501a9e83fc0546cba866f314d6de0949
nl1010 <yanlu1990@hotmail.co.uk>
2015-02-10 21:51:50 +0000

    rotate func implementation done , and also make some structure files

--- utils.py
+++ utils.py
@@ -2,37 +2,58 @@ import brickpi
 import time
 from math import pi
 
-WHEEL_RADIUS = 1.5
-ROBOT_RADIUS = 5.0
+class RobotUtility:
+  #constructor
+  def __init__(self):
+    self.WHEEL_RADIUS = 1.5
+    self.ROBOT_RADIUS = 5.5
+    self.PERC_MOD_WHEEL = 1 #init
+    self.PERC_MOD_BOOST = 1 #init 
+    self.DEGS = 0
 
-motors = [0,1]
+  #set configurations from callibration result 
+  def set_drift_callibration (self,mode_wheel,boost,degs):
+    self.PERC_MOD_WHEEL = mdoel_wheel 
+    self.PERC_MOD_BOOST = boost
+    self.DEGS = degs 
 
-interface = brickpi.Interface()
-interface.initialize()
-interface.motorEnable(motors[0])
-interface.motorEnable(motors[1])
-motorParam0 = interface.MotorAngleControllerParameters()
-motorParam1 = interface.MotorAngleControllerParameters()  
 
-motorParam0.maxRotationAcceleration = 7.0
-motorParam0.maxRotationSpeed = 12.0
-motorParam0.feedForwardGain = 255/20.0
-motorParam0.minPWM = 18.0
-motorParam0.pidParameters.minOutput = -255
-motorParam0.pidParameters.maxOutput = 255
-motorParam0.pidParameters.k_p = 20.0
-motorParam0.pidParameters.k_i = 0.0
-motorParam0.pidParameters.k_d = 0.0
+
+
+# WHEEL_RADIUS = 1.5
+# ROBOT_RADIUS = 5.0
+
+# motors = [0,1]
+
+# interface = brickpi.Interface()
+# interface.initialize()
+# interface.motorEnable(motors[0])
+# interface.motorEnable(motors[1])
+# motorParam0 = interface.MotorAngleControllerParameters()
+# motorParam1 = interface.MotorAngleControllerParameters()  
+
+# motorParam0.maxRotationAcceleration = 7.0
+# motorParam0.maxRotationSpeed = 12.0
+# motorParam0.feedForwardGain = 255/20.0
+# motorParam0.minPWM = 18.0
+# motorParam0.pidParameters.minOutput = -255
+# motorParam0.pidParameters.maxOutput = 255
+# motorParam0.pidParameters.k_p = 20.0
+# motorParam0.pidParameters.k_i = 0.0
+# motorParam0.pidParameters.k_d = 0.0
   
-motorParam1.maxRotationAcceleration = 7.0 
-motorParam1.maxRotationSpeed = 12.0
-motorParam1.feedForwardGain = 255/20.0
-motorParam1.minPWM = 18.0
-motorParam1.pidParameters.minOutput = -255
-motorParam1.pidParameters.maxOutput = 255 
-motorParam1.pidParameters.k_p = 20.0
-motorParam1.pidParameters.k_i = 0.0 
-motorParam1.pidParameters.k_d = 0.0 
+# motorParam1.maxRotationAcceleration = 7.0 
+# motorParam1.maxRotationSpeed = 12.0
+# motorParam1.feedForwardGain = 255/20.0
+# motorParam1.minPWM = 18.0
+# motorParam1.pidParameters.minOutput = -255
+# motorParam1.pidParameters.maxOutput = 255 
+# motorParam1.pidParameters.k_p = 20.0
+# motorParam1.pidParameters.k_i = 0.0 
+# motorParam1.pidParameters.k_d = 0.0 
+
+
+
 
 # '''Move distance cm forward'''
 # def move(distance):
@@ -46,7 +67,7 @@ motorParam1.pidParameters.k_d = 0.0
 #     if motorAngles :
 #       print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
 
-#My intergration of move 
+#My intergration of move forward 
 def move (distance)(self,distance,interface ,motorParams,verbose=True):
   motors = [0,1]
   # to control the motor angle correctly ,
@@ -55,7 +76,7 @@ def move (distance)(self,distance,interface ,motorParams,verbose=True):
   interface.setMotorAngleControllerParameters(0.motorParam1)
 
   # set target by using equation (?)
-  interface.increaseMotorAngleReferences(motors, [(distance * )])
+  interface.increaseMotorAngleReferences(motors, [(distance * self.PERC_MOD_BOOST)]/self.WHEEL_RADIUS,self.PERC_MOD_WHEEL * distance * self.PERC_MOD_BOOST/self.WHEEL_RADIUS)
 
   #start process to approach the target by using While Not 
   while not interface.motorAngleReferencesReached (motors) :
@@ -70,8 +91,10 @@ def rotate(radians):
   interface.setMotorAngleControllerParameters(motors[0],motorParam0)
   interface.setMotorAngleControllerParameters(motors[1],motorParam1)
   
-
-  angle = ROBOT_RADIUS * pi / WHEEL_RADIUS
+  #angle = ROBOT_RADIUS * pi / WHEEL_RADIUS 
+  #my implementation of angle 
+  radiance = radiance + self.DEGS * pi / 180 #add up two raidicance gives final radiance  <?>then what is self.DEG?
+  angle = self.ROBOT_RADIUS * radiance / self.WHEEL_RADIUS #<?> what does this equation comes from? 
 
   interface.increaseMotorAngleReferences(motors, [angle, -angle])
  
